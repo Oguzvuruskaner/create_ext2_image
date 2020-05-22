89c98131d17afb3cc85ca8f481af9e730501214f
Oguz Vuruskaner <oguzvuruskaner@gmail.com>
2020-05-22 16:11:32 +0300

    Deleted subprocess commands.

--- __main__.py
+++ __main__.py
@@ -10,7 +10,7 @@ from base64 import b64encode
 import faker
 
 
-MAX_DEPTH = 12
+MAX_DEPTH = 8
 FAKER = faker.Faker()
 
 get_filename = lambda : FAKER.first_name_male()
@@ -39,13 +39,22 @@ def create_directory(path,depth,directory_probability = 0.6,file_probability = 0
         os.mkdir(os.path.join(path,dir_basename))
         create_directory(os.path.join(path,dir_basename),depth+1)
 
+    i = 0
+
     while(bernoulli(file_probability)):
-        with open(os.path.join(path,get_filename()),"w") as fp:
-            random_bytes = os.urandom(get_contentsize())
-            fp.write(b64encode(random_bytes).decode("utf-8"))
-
-
-
+        i += 1
+        file_path = os.path.join(path,get_filename())
+        try:
+            
+            with open(file_path,"w") as fp:
+                random_bytes = os.urandom(get_contentsize())
+                fp.write(b64encode(random_bytes).decode("utf-8"))
+        
+        except FileExistsError:
+            file_path += str(i)
+            with open(file_path,"w") as fp:
+                random_bytes = os.urandom(get_contentsize())
+                fp.write(b64encode(random_bytes).decode("utf-8"))
 
 
 
@@ -55,8 +64,11 @@ if __name__ == "__main__":
     random.seed(time.time())
 
     entry_basename = get_filename()
-    os.mkdir(os.path.join(os.getcwd(),entry_basename))
-    create_directory(os.path.join(os.getcwd(),entry_basename),1)
+
+    root_path = os.path.join(os.getcwd(),entry_basename)
+    os.mkdir(root_path)
+    create_directory(root_path,1)
+
     
 
 
